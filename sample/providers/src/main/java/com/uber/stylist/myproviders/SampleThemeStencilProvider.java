c20133d90437c6849c825f48a4b16fdbd424651b
James Barr <jbarr@uber.com>
2018-08-16 01:54:49 -0700

    Add sample library with providers module to demo usage

---
+++ sample/providers/src/main/java/com/uber/stylist/myproviders/SampleThemeStencilProvider.java
@@ -0,0 +1,56 @@
+package com.uber.stylist.myproviders;
+
+import android.support.annotation.NonNull;
+
+import com.commit451.uresourcespoet.StyleItem;
+import com.google.auto.service.AutoService;
+import com.uber.stylist.api.StyleItemGroup;
+import com.uber.stylist.api.ThemeStencil;
+import com.uber.stylist.api.ThemeStencilProvider;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AutoService(ThemeStencilProvider.class)
+public class SampleThemeStencilProvider implements ThemeStencilProvider {
+
+  private StyleItemGroup basicAppColors = new StyleItemGroup(
+      new StyleItem("colorPrimary", "@color/colorPrimary"),
+      new StyleItem("colorPrimaryDark", "@color/colorPrimaryDark"),
+      new StyleItem("colorAccent", "@color/colorAccent")
+  );
+
+  private StyleItemGroup textSizes = new StyleItemGroup(
+      new StyleItem("textSizeSmall", "12dp"),
+      new StyleItem("textSizeMedium","16dp"),
+      new StyleItem("textSizeLarge", "20dp")
+  );
+
+  private StyleItemGroup dialogAttrs = new StyleItemGroup(
+      new StyleItem("dialogSpecificAttr1", "foo"),
+      new StyleItem("dialogSpecificAttr2", "bar")
+  );
+
+  @NonNull
+  @Override
+  public Set<ThemeStencil> stencils() {
+    return new LinkedHashSet<>(Arrays.asList(
+        new ThemeStencil("Theme.Sample", "Theme.AppCompat"),
+        new ThemeStencil("Theme.Sample.Light", "Theme.AppCompat.Light"),
+        new ThemeStencil("Theme.Sample.Dialog", "Theme.AppCompat.Dialog", dialogAttrs),
+        new ThemeStencil("Theme.Sample.Light.Dialog", "Theme.AppCompat.Light.Dialog", dialogAttrs)
+    ));
+  }
+
+  @NotNull
+  @Override
+  public Set<StyleItemGroup> globalStyleItemGroups() {
+    return new LinkedHashSet<>(Arrays.asList(
+        basicAppColors,
+        textSizes
+    ));
+  }
+}
