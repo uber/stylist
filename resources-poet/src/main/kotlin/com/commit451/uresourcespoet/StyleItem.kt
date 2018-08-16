34cb1eda83e20a6428e90242eb8593eed7015a06
James Barr <jbarr@uber.com>
2018-08-16 01:45:07 -0700

    Add cleaned up resources-poet

---
+++ resources-poet/src/main/kotlin/com/commit451/uresourcespoet/StyleItem.kt
@@ -0,0 +1,8 @@
+package com.commit451.uresourcespoet
+
+/**
+ * An item within a style, like
+ *
+ * <item name="android:windowBackground">@android:color/black</item>
+ */
+data class StyleItem(val name: String, val value: String)
