cb73fdcc49368194a22493babb27f1a21b80e9d4
James Barr <jbarr@uber.com>
2018-08-16 19:24:40 -0700

    Add tests for generated Android XML themes

---
+++ stylist-core/src/test/kotlin/com/uber/stylist/StylistTest.kt
@@ -0,0 +1,237 @@
+package com.uber.stylist
+
+import com.commit451.uresourcespoet.StyleItem
+import com.google.common.io.Files
+import com.google.common.truth.Truth.assertThat
+import com.uber.stylist.api.StyleItemGroup
+import com.uber.stylist.api.ThemeStencil
+import org.junit.Test
+
+class StylistTest {
+
+  companion object {
+    private const val THEME_XML_FILE_NAME = "themes_stylist_generated.xml"
+
+    private const val THEMES_NO_THEMES = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources/>
+"""
+
+    private const val THEMES_NO_STYLE_GROUPS = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources>
+  <style name="Theme.Test" parent="Theme.AppCompat"/>
+  <style name="Theme.Test.Light" parent="Theme.AppCompat.Light"/>
+  <style name="Theme.Test.Dialog" parent="Theme.AppCompat.Dialog"/>
+  <style name="Theme.Test.Light.Dialog" parent="Theme.AppCompat.Light.Dialog"/>
+</resources>
+"""
+
+    private const val THEMES_WITH_STYLE_GROUP = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources>
+  <style name="Theme.Test" parent="Theme.AppCompat">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+  </style>
+  <style name="Theme.Test.Light" parent="Theme.AppCompat.Light">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+  </style>
+  <style name="Theme.Test.Dialog" parent="Theme.AppCompat.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+  </style>
+  <style name="Theme.Test.Light.Dialog" parent="Theme.AppCompat.Light.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+  </style>
+</resources>
+"""
+
+    private const val THEMES_WITH_STYLE_GROUPS = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources>
+  <style name="Theme.Test" parent="Theme.AppCompat">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+  <style name="Theme.Test.Light" parent="Theme.AppCompat.Light">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+  <style name="Theme.Test.Dialog" parent="Theme.AppCompat.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+  <style name="Theme.Test.Light.Dialog" parent="Theme.AppCompat.Light.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+</resources>
+"""
+
+    private const val THEMES_WITH_STYLE_GROUPS_PLUS_ADDED = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources>
+  <style name="Theme.Test" parent="Theme.AppCompat">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+  <style name="Theme.Test.Light" parent="Theme.AppCompat.Light">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+  </style>
+  <style name="Theme.Test.Dialog" parent="Theme.AppCompat.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+    <item name="dialogSpecificAttr1">foo</item>
+    <item name="dialogSpecificAttr2">bar</item>
+  </style>
+  <style name="Theme.Test.Light.Dialog" parent="Theme.AppCompat.Light.Dialog">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+    <item name="colorAccent">@color/colorAccent</item>
+    <item name="textSizeSmall">12dp</item>
+    <item name="textSizeMedium">16dp</item>
+    <item name="textSizeLarge">20dp</item>
+    <item name="dialogSpecificAttr1">foo</item>
+    <item name="dialogSpecificAttr2">bar</item>
+  </style>
+</resources>
+"""
+
+    private val THEMES_UNFORMATTED = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<resources>
+<style name="Theme.Test" parent="Theme.AppCompat">
+<item name="colorPrimary">@color/colorPrimary</item>
+<item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+<item name="colorAccent">@color/colorAccent</item>
+</style>
+<style name="Theme.Test.Light" parent="Theme.AppCompat.Light">
+<item name="colorPrimary">@color/colorPrimary</item>
+<item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+<item name="colorAccent">@color/colorAccent</item>
+</style>
+<style name="Theme.Test.Dialog" parent="Theme.AppCompat.Dialog">
+<item name="colorPrimary">@color/colorPrimary</item>
+<item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+<item name="colorAccent">@color/colorAccent</item>
+</style>
+<style name="Theme.Test.Light.Dialog" parent="Theme.AppCompat.Light.Dialog">
+<item name="colorPrimary">@color/colorPrimary</item>
+<item name="colorPrimaryDark">@color/colorPrimaryDark</item>
+<item name="colorAccent">@color/colorAccent</item>
+</style>
+</resources>""".split("\n").joinToString(separator = "")
+
+    private val basicAppColors = StyleItemGroup(
+        StyleItem("colorPrimary", "@color/colorPrimary"),
+        StyleItem("colorPrimaryDark", "@color/colorPrimaryDark"),
+        StyleItem("colorAccent", "@color/colorAccent")
+    )
+
+    private val textSizes = StyleItemGroup(
+        StyleItem("textSizeSmall", "12dp"),
+        StyleItem("textSizeMedium", "16dp"),
+        StyleItem("textSizeLarge", "20dp")
+    )
+
+    private val dialogAttrs = StyleItemGroup(
+        StyleItem("dialogSpecificAttr1", "foo"),
+        StyleItem("dialogSpecificAttr2", "bar")
+    )
+
+    val themeStencils: Set<ThemeStencil> = linkedSetOf(
+        ThemeStencil("Theme.Test", "Theme.AppCompat"),
+        ThemeStencil("Theme.Test.Light", "Theme.AppCompat.Light"),
+        ThemeStencil("Theme.Test.Dialog", "Theme.AppCompat.Dialog"),
+        ThemeStencil("Theme.Test.Light.Dialog", "Theme.AppCompat.Light.Dialog")
+    )
+
+    val themeStencilsAddedStyles: Set<ThemeStencil> = linkedSetOf(
+        ThemeStencil("Theme.Test", "Theme.AppCompat"),
+        ThemeStencil("Theme.Test.Light", "Theme.AppCompat.Light"),
+        ThemeStencil("Theme.Test.Dialog", "Theme.AppCompat.Dialog", dialogAttrs),
+        ThemeStencil("Theme.Test.Light.Dialog", "Theme.AppCompat.Light.Dialog", dialogAttrs)
+    )
+  }
+
+  @Test
+  fun testStylist_withNoThemes_shouldGenerateExpectedXml() {
+    testXml(emptySet(), emptySet(), THEMES_NO_THEMES)
+  }
+
+  // failing
+  @Test
+  fun testStylist_withNoStyleGroups_shouldGenerateExpectedXml() {
+    testXml(themeStencils, linkedSetOf(), THEMES_NO_STYLE_GROUPS)
+  }
+
+  // failing
+  @Test
+  fun testStylist_withSingleStyleGroup_shouldGenerateExpectedXml() {
+    testXml(themeStencils, linkedSetOf(basicAppColors), THEMES_WITH_STYLE_GROUP)
+  }
+
+  @Test
+  fun testStylist_withMultipleStyleGroups_shouldGenerateExpectedXml() {
+    testXml(themeStencils, linkedSetOf(basicAppColors, textSizes), THEMES_WITH_STYLE_GROUPS)
+  }
+
+  @Test
+  fun testStylist_withMultipleStyleGroupsPlusAdded_shouldGenerateExpectedXml() {
+    testXml(themeStencilsAddedStyles, linkedSetOf(basicAppColors, textSizes), THEMES_WITH_STYLE_GROUPS_PLUS_ADDED)
+  }
+
+  @Test
+  fun testStylist_withFormatFalse_shouldGenerateExpectedXml() {
+    testXml(themeStencils, linkedSetOf(basicAppColors), THEMES_UNFORMATTED, formatSource = false)
+  }
+
+  private fun testXml(stencils: Set<ThemeStencil>, globalStyleGroups: Set<StyleItemGroup>, expectedXml: String, formatSource: Boolean = true) {
+    val outputDir = Files.createTempDir()
+
+
+    outputDir.resolve("values").run {
+      deleteRecursively()
+
+      Stylist.generateThemesForStencils(stencils, globalStyleGroups, outputDir, formatSource)
+
+      val xmlFileNames = listFiles().map { it.name }.toList()
+      assertThat(xmlFileNames).containsExactly(THEME_XML_FILE_NAME)
+
+      val generatedThemesXml = listFiles().first().readText()
+      assertThat(generatedThemesXml).isEqualTo(expectedXml)
+
+    }
+    outputDir.deleteRecursively()
+  }
+}
